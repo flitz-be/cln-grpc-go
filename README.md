e80f67515e1f9ade3c2de049ce3f2d5bd708d8b1
kiwiidb <kwinten.debacker@hotmail.com>
2022-04-13 14:35:41 +0200

    add mutual tls

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Golang bindings for the CLN gRPC client
 
+Generating from [CLN's proto files](https://github.com/ElementsProject/lightning/tree/master/cln-grpc/proto):
+
 ```
 protoc --go_out=cln --go_opt=paths=source_relative \
     --go-grpc_out=cln --go-grpc_opt=paths=source_relative primitives.proto
@@ -8,4 +10,10 @@ protoc --go_out=cln --go_opt=paths=source_relative \
 go mod tidy
 ```
 
-An example client can be found in the `example` folder.
+An example client can be found in the `example` folder. Make sure that your node has gRPC enabled
+(`--grpc-port=port`), and change the host name in `example/main.go` or use port-forwarding.
+To use the client, you need the following files from your node's `.lightning` directory:
+
+- ca.pem  
+- client-key.pem 
+- client.pem
--- example/main.go
+++ example/main.go
@@ -2,14 +2,63 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/flitz-be/cln-grpc-go/cln"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
+func loadTLSCredentials(caCertPath, clientCertPath, clientKeyPath string) (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair(clientCertPath, clientKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func main() {
-	conn, err := grpc.Dial("localhost:8080")
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	credentials, err := loadTLSCredentials(
+		fmt.Sprintf("%s/.lightning/ca.pem", dirname),
+		fmt.Sprintf("%s/.lightning/client.pem", dirname),
+		fmt.Sprintf("%s/.lightning/client-key.pem", dirname))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(credentials),
+	}
+
+	conn, err := grpc.Dial("localhost:9737", opts...)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
